d5bbee137e2221412cb05d2274b3fb81e680d39a
dedalo6 <aperezdmor@educacion.navarra.es>
2023-10-29 22:36:32 +0100

    Copiado esta, pero no funciona
    Un tochaco infernal para abrir dos pantallitas

---
+++ Navegación de Aritz Perez de Ciriza/Navegación de Aritz Perez de Ciriza/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Navegación_de_Aritz_Perez_de_Ciriza
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Navegación de Aritz Perez de Ciriza/Navegación de Aritz Perez de Ciriza/Window2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Navegación_de_Aritz_Perez_de_Ciriza
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow(object sender, RoutedEventArgs e)
+        {
+            MainWindow AbrirMainWindow = new MainWindow();
+            this.Close();
+            AbrirMainWindow.Show();
+        }
+
+        private void Boton2_AbrirPagina(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(new Page1());
+        }
+
+       
+
+    }
+}
