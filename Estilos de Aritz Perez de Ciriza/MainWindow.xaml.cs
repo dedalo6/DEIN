539b055166a33bea7b749b8235008bf505539218
dedalo6 <aperezdmor@educacion.navarra.es>
2023-11-05 22:40:17 +0100

    Funciona

--- Estilos de Aritz Perez de Ciriza/MainWindow.xaml.cs
+++ Estilos de Aritz Perez de Ciriza/MainWindow.xaml.cs
@@ -25,6 +25,42 @@ namespace Estilos_de_Aritz_Perez_de_Ciriza
             InitializeComponent();
         }
 
-        
-    }
+        private void ButtonMesas_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 AbrirVentana1_Mesas = new Window1();
+
+            this.Close();
+
+            AbrirVentana1_Mesas.Show();
+        }
+
+        private void ButtonTimetable_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 AbrirVentana2_Timetable = new Window2();
+
+            this.Close();
+
+            AbrirVentana2_Timetable.Show();
+        }
+
+        private void ButtonRecetas_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 AbrirVentana3_Recetas = new Window3();
+
+            this.Close();
+
+            AbrirVentana3_Recetas.Show();
+        }
+
+        private void ButtonMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow AbrirVentana0_Main = new MainWindow();
+
+            this.Close();
+
+            AbrirVentana0_Main.Show();
+        }
+
+    }  
+    
 }
